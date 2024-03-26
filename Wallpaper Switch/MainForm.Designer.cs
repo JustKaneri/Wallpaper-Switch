91a186653dfac4660b8088ef63feed5250859c10
justkaneri <justkaneri@yandex.ru>
2024-03-26 21:08:14 +0300

    Создание контроль панели

---
+++ Wallpaper Switch/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Wallpaper_Switch
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ControlPanel = new System.Windows.Forms.Panel();
+            this.BtnMinWindow = new System.Windows.Forms.Label();
+            this.BtnClose = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ControlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ControlPanel
+            // 
+            this.ControlPanel.Controls.Add(this.BtnMinWindow);
+            this.ControlPanel.Controls.Add(this.BtnClose);
+            this.ControlPanel.Controls.Add(this.label1);
+            this.ControlPanel.Controls.Add(this.pictureBox1);
+            this.ControlPanel.Location = new System.Drawing.Point(0, 0);
+            this.ControlPanel.Name = "ControlPanel";
+            this.ControlPanel.Size = new System.Drawing.Size(1198, 55);
+            this.ControlPanel.TabIndex = 0;
+            this.ControlPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ControlPanel_MouseDown);
+            this.ControlPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ControlPanel_MouseMove);
+            this.ControlPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ControlPanel_MouseUp);
+            // 
+            // BtnMinWindow
+            // 
+            this.BtnMinWindow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMinWindow.Font = new System.Drawing.Font("Constantia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnMinWindow.ForeColor = System.Drawing.Color.White;
+            this.BtnMinWindow.Location = new System.Drawing.Point(1071, 0);
+            this.BtnMinWindow.Name = "BtnMinWindow";
+            this.BtnMinWindow.Size = new System.Drawing.Size(58, 55);
+            this.BtnMinWindow.TabIndex = 4;
+            this.BtnMinWindow.Text = "_";
+            this.BtnMinWindow.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnMinWindow.Click += new System.EventHandler(this.BtnMinWindow_Click);
+            this.BtnMinWindow.MouseEnter += new System.EventHandler(this.BtnControl_MouseEnter);
+            this.BtnMinWindow.MouseLeave += new System.EventHandler(this.BtnControl_MouseLeave);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.Font = new System.Drawing.Font("Constantia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnClose.ForeColor = System.Drawing.Color.White;
+            this.BtnClose.Location = new System.Drawing.Point(1135, 0);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(58, 55);
+            this.BtnClose.TabIndex = 3;
+            this.BtnClose.Text = "X";
+            this.BtnClose.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            this.BtnClose.MouseEnter += new System.EventHandler(this.BtnControl_MouseEnter);
+            this.BtnClose.MouseLeave += new System.EventHandler(this.BtnControl_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Constantia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(72, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wallpaper Switch";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(19)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(1197, 605);
+            this.Controls.Add(this.ControlPanel);
+            this.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wallpaper Switch";
+            this.ControlPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ControlPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label BtnClose;
+        private System.Windows.Forms.Label BtnMinWindow;
+    }
+}
+
---
+++ Wallpaper Switch/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wallpaper_Switch
+{
+    public partial class MainForm : Form
+    {
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Control Panel
+        private void BtnControl_MouseEnter(object sender, EventArgs e)
+        {
+            var btn = ((Label)sender);
+
+            btn.Font = new Font(btn.Font.FontFamily,btn.Font.Size+2,btn.Font.Style);
+        }
+
+        private void BtnControl_MouseLeave(object sender, EventArgs e)
+        {
+            var btn = ((Label)sender);
+
+            btn.Font = new Font(btn.Font.FontFamily, btn.Font.Size - 2, btn.Font.Style);
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnMinWindow_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ControlPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset;
+            int yOffset;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset = -e.Y - SystemInformation.CaptionHeight - SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset, yOffset);
+                isMouseDown = true;
+            }
+        }
+
+        private void ControlPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y+ControlPanel.Height/2);
+                Location = mousePos;
+            }
+        }
+
+        private void ControlPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+        #endregion
+
+    }
+}
