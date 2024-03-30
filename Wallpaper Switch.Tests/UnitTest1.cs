c469f66d212da1361a05b9f1c425979726c1ca39
justkaneri <justkaneri@yandex.ru>
2024-03-30 19:19:52 +0300

    Cоздание core проекта и юнит тесты

---
+++ Wallpaper Switch.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Wallpaper_Switch.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
