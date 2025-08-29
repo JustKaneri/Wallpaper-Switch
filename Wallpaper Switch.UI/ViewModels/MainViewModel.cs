903b92e495d6fcc491c76046a63945fba0645950
justkaneri <justkaneri@yandex.ru>
2025-08-29 20:59:08 +0300

    feat: создание нового проекта UI

---
+++ Wallpaper Switch.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Wallpaper_Switch.UI.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Wallpaper Switch.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Wallpaper_Switch.UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
