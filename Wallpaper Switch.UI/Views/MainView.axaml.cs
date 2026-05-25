748347376d5258db58e527bc1eda3dd1365a783f
justkaneri <justkaneri@yandex.ru>
2026-05-25 13:28:36 +0300

    Control panel

--- Wallpaper Switch.UI/Views/MainView.axaml.cs
+++ Wallpaper Switch.UI/Views/MainView.axaml.cs
@@ -1,11 +1,36 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace Wallpaper_Switch.UI.Views;
 
 public partial class MainView : UserControl
 {
+    private Button _activeButton;
+
     public MainView()
     {
         InitializeComponent();
+        _activeButton = BtnHome;
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        DisableButton(_activeButton);
+        _activeButton = sender as Button;
+        EnableButton(_activeButton);
+    }
+
+    private void DisableButton(Button button)
+    {
+        button.Background = null;
+        button.BorderBrush = null;
+        button.BorderThickness = new Avalonia.Thickness(0, 0, 0, 0);
+    }
+
+    private void EnableButton(Button button)
+    {
+        button.BorderBrush = Brush.Parse("#5B8CFF");
+        button.BorderThickness = new Avalonia.Thickness(3, 0, 0, 0);
+        button.Background = Brush.Parse("#283036");
     }
 }
