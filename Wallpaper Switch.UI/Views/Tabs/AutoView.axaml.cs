267d030c9f9a881a7a270484bfc90e6842c75e19
justkaneri <justkaneri@yandex.ru>
2026-06-16 13:33:43 +0300

    Стиль для выбора времени таймера

--- Wallpaper Switch.UI/Views/Tabs/AutoView.axaml.cs
+++ Wallpaper Switch.UI/Views/Tabs/AutoView.axaml.cs
@@ -1,13 +1,26 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Diagnostics;
 
 namespace Wallpaper_Switch.UI;
 
 public partial class AutoView : UserControl
 {
+    private Button _activeTimerData;
+
     public AutoView()
     {
         InitializeComponent();
+        _activeTimerData = Btn1m;
+        _activeTimerData.Classes.Set("active", true);
+    }
+
+    private void TimerSelector_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        _activeTimerData.Classes.Set("active", false);
+        _activeTimerData = sender as Button;
+        _activeTimerData.Classes.Set("active", true);
+        Debug.WriteLine((sender as Button).Tag);
     }
 }
